fc1a7b8349ae9f9c13519092ce7f3609f783238c
ASHISH BISHT <ashish23072002@gmail.com>
2023-04-19 20:46:00 +0530

    modified today 19-04

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,15 +3,24 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
+  const ProtectedRoute = ({ children }) => {
+    const { user } = useContext(AuthContext);
 
+    if (!user) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
   return (
     <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
