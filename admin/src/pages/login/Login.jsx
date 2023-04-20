692dfbbd50925e61d29a3c69981fbbe55bbbaef7
ASHISH BISHT <ashish23072002@gmail.com>
2023-04-20 10:40:53 +0530

    modified today 20-04

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -25,20 +25,13 @@ const Login = () => {
     dispatch({ type: "LOGIN_START" });
     try {
       const res = await axios.post("/auth/login", credentials);
-      if (res.data.isAdmin) {
-        dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
-
-        navigate("/");
-      } else {
-        dispatch({
-          type: "LOGIN_FAILURE",
-          payload: { message: "You are not allowed!" },
-        });
-      }
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+      navigate("/")
     } catch (err) {
       dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
     }
   };
+
 
 
 
